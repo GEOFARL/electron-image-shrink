adee12220669579699adbafcab95d4741c4554e6
Maxim <toper.one111@gmail.com>
2023-03-26 11:00:36 +0300

    Create a Menu Template

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 // Set environment
 process.env.NODE_ENV = 'development';
@@ -22,7 +22,27 @@ function createMainWindow() {
   mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
 
-app.on('ready', createMainWindow);
+const menu = [
+  ...(isMac ? [{ role: 'appMenu' }] : []),
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        click: () => app.quit(),
+      },
+    ],
+  },
+];
+
+app.on('ready', () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on('closed', () => (mainWindow = null));
+});
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
